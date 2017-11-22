9cea76caa0549cfd6651294ffdd9d7b2aea28be6
nick-holt <naholt01@gmail.com>
2017-11-22 12:15:52 -0500

    Added some compatibility for app

--- fp_rankings_scraper.R
+++ fp_rankings_scraper.R
@@ -7,37 +7,37 @@ library(tidyverse)
 library(stringr)
 
 # URL info
-        qb <- "https://www.fantasypros.com/nfl/rankings/qb.php"
-        k <- "https://www.fantasypros.com/nfl/rankings/k.php"
-        dst <- "https://www.fantasypros.com/nfl/rankings/dst.php"
-        idp <- "https://www.fantasypros.com/nfl/rankings/idp.php"
-        dl <- "https://www.fantasypros.com/nfl/rankings/dl.php"
-        lb <- "https://www.fantasypros.com/nfl/rankings/lb.php"
-        db <- "https://www.fantasypros.com/nfl/rankings/db.php"
 
         # standard scoring
-        rb <- "https://www.fantasypros.com/nfl/rankings/rb.php"
-        wr <- "https://www.fantasypros.com/nfl/rankings/wr.php"
-        te <- "https://www.fantasypros.com/nfl/rankings/te.php"
-        flex <- "https://www.fantasypros.com/nfl/rankings/flex.php"
-        qbflex <- "https://www.fantasypros.com/nfl/rankings/qb-flex.php"
+        qb_standard <- "https://www.fantasypros.com/nfl/rankings/qb.php"
+        k_standard <- "https://www.fantasypros.com/nfl/rankings/k.php"
+        dst_standard <- "https://www.fantasypros.com/nfl/rankings/dst.php"
+        idp_standard <- "https://www.fantasypros.com/nfl/rankings/idp.php"
+        dl_standard <- "https://www.fantasypros.com/nfl/rankings/dl.php"
+        lb_standard <- "https://www.fantasypros.com/nfl/rankings/lb.php"
+        db_standard <- "https://www.fantasypros.com/nfl/rankings/db.php"
+        rb_standard <- "https://www.fantasypros.com/nfl/rankings/rb.php"
+        wr_standard <- "https://www.fantasypros.com/nfl/rankings/wr.php"
+        te_standard <- "https://www.fantasypros.com/nfl/rankings/te.php"
+        flex_standard <- "https://www.fantasypros.com/nfl/rankings/flex.php"
+        qb_flex_standard <- "https://www.fantasypros.com/nfl/rankings/qb-flex.php"
         
         # ppr
-        rbppr <- "https://www.fantasypros.com/nfl/rankings/ppr-rb.php"
-        wrppr <- "https://www.fantasypros.com/nfl/rankings/ppr-wr.php"
-        teppr <- "https://www.fantasypros.com/nfl/rankings/ppr-te.php"
-        flexppr <- "https://www.fantasypros.com/nfl/rankings/ppr-flex.php"
-        qbflexppr <- "https://www.fantasypros.com/nfl/rankings/ppr-qb-flex.php"
+        rb_ppr <- "https://www.fantasypros.com/nfl/rankings/ppr-rb.php"
+        wr_ppr <- "https://www.fantasypros.com/nfl/rankings/ppr-wr.php"
+        te_ppr <- "https://www.fantasypros.com/nfl/rankings/ppr-te.php"
+        flex_ppr <- "https://www.fantasypros.com/nfl/rankings/ppr-flex.php"
+        qb_flex_ppr <- "https://www.fantasypros.com/nfl/rankings/ppr-qb-flex.php"
         
         # half-point per reception
-        rbhalf <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-rb.php"
-        wrhalf <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-wr.php"
-        tehalf <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-te.php"
-        flexhalf <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-flex.php"
-        qbflexhalf <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-qb-flex.php"
+        rb_half_ppr <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-rb.php"
+        wr_half_ppr <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-wr.php"
+        te_half_ppr <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-te.php"
+        flex_half_ppr <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-flex.php"
+        qb_flex_half_ppr <- "https://www.fantasypros.com/nfl/rankings/half-point-ppr-qb-flex.php"
 
 # function to scrape and clean rankings data 
-scrape_ranks <- function(type_url = qb) {
+scrape_ranks <- function(type_url) {
         fp_data <- getURL(type_url)
         ranks <- readHTMLTable(fp_data, header = T, stringsAsFactors = F)
         ranks <- data.frame(ranks$`rank-data`)
@@ -47,4 +47,5 @@ scrape_ranks <- function(type_url = qb) {
         ranks <- ranks %>%
                 na.omit() %>%
                 mutate(player = str_sub(player, 1, -4))
+        return(ranks)
 }
